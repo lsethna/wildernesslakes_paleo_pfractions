d3c55ad3aa780cba69257796d0dff505367df6d2
Lienne Sethna <30530947+lsethna@users.noreply.github.com>
2024-09-10 15:10:24 -0500

    Format dataframes and merge
    Change column names to match (lake names and depth) and merge on lake and depth

--- Master dataset/Create master dataset.R
+++ Master dataset/Create master dataset.R
@@ -7,7 +7,7 @@ rm(list=ls())
 librarian::shelf(tidyverse, googledrive,readxl)
 
 getwd()
-setwd("C:/Users/lsethna_smm/Documents/GitHub/wildernesslakes_paleo_pfractions") #change this to match local GitHub folder
+setwd("C:/Users/lsethna_smm/Documents/GitHub/wildernesslakes_paleo_pfractions/Master dataset") #change this to match local GitHub folder
 
 ## ----------------------------------- ##
 # Download data ----
@@ -34,14 +34,89 @@ wanted_files <- googledrive::drive_ls(path = bsi_url) %>%
                             "WL_pigments_allcores.csv",
                             "Pfrac_mass_focuscorrect" #saved as Google sheet, no file extension needed
                             ))
-
 # Check those files
 wanted_files
-
 # Create a folder to download data into
 dir.create(path = file.path("raw_data"), showWarnings = F)
-
 # Download that data
 purrr::walk2(.x = wanted_files$name, .y = wanted_files$id,
              .f = ~ googledrive::drive_download(file = .y, overwrite = T,
                                                 path = file.path("raw_data", .x)))
+
+## ----------------------------------- ##
+# Read in data ----
+## ----------------------------------- ##
+bsi <- read_excel("raw_data/WL_BSi_all.xlsx")
+dates_dmar <- read.csv("raw_data/sections_interp_year_dmar_30July2024.csv")
+loi <- read_excel("raw_data/WL_LOI_allcores.xlsx") %>% janitor::clean_names()
+pfracs <- read_excel("raw_data/Pfrac_mass_focuscorrect.xlsx")
+pigments <- read.csv("raw_data/WL_pigments_allcores.csv")
+
+#check the data
+glimpse(bsi)
+glimpse(dates_dmar)
+glimpse(loi)
+glimpse(pfracs)
+glimpse(pigments)
+
+## ----------------------------------- ##
+# Match columns and lake names ----
+## ----------------------------------- ##
+unique(bsi$lake) #master lake name convention: lowercase, no spaces, abbreviate East and West as e and w
+unique(dates_dmar$lake)
+unique(loi$lake)
+unique(pfracs$lake)
+unique(pigments$lake)
+
+#format column names to match
+#lake, depth, analyte before name...
+bsi_v2 <- bsi %>% select(!c(date,dmar,flux_g)) %>% janitor::clean_names() %>%
+  rename(flux_sio2_mg=flux_mg,
+         sio2_wt_percent=wt_percent) 
+glimpse(bsi_v2)
+
+dates_dmar_v2 <- 
+  dates_dmar %>% select(lake,year_loess,dmar_loess,base) %>%
+  rename(depth=base)%>%
+  mutate(lake=case_when(lake=="Burnt"~"burnt",
+                        lake=="Dunnigan"~"dunnigan",
+                        lake=="East Twin"~"etwin",
+                        lake=="Elbow"~"elbow",
+                        lake=="Finger"~"finger",
+                        lake=="Flame"~"flame",
+                        lake=="Smoke"~"smoke",
+                        lake=="West Twin"~"wtwin"))
+
+loi_v2 <- loi %>% select(!c(top)) %>%
+  rename(depth=base)%>%
+  mutate(lake=case_when(lake=="Burnt"~"burnt",
+                        lake=="Dunnigan"~"dunnigan",
+                        lake=="E Twin"~"etwin",
+                        lake=="Elbow"~"elbow",
+                        lake=="Finger"~"finger",
+                        lake=="Flame"~"flame",
+                        lake=="Smoke"~"smoke",
+                        lake=="W Twin"~"wtwin"))
+
+pfracs_v2 <- pfracs %>% select(!c(`...1`,id,notes,DMAR,year)) %>%
+  rename(depth=depth_base)
+
+pigments_v2 <- pigments %>% select(!c(X,sample_depth,extraction_weight_mg,percent_organic)) %>%
+  mutate(lake=case_when(lake=="Burnt"~"burnt",
+                        lake=="Dunnigan"~"dunnigan",
+                        lake=="ETwin"~"etwin",
+                        lake=="Elbow"~"elbow",
+                        lake=="Finger"~"finger",
+                        lake=="Flame"~"flame",
+                        lake=="Smoke"~"smoke",
+                        lake=="WTwin"~"wtwin"))
+
+## ----------------------------------- ##
+# Merge together ----
+## ----------------------------------- ##
+master_v1 <- dates_dmar_v2 %>% 
+  full_join(loi_v2) %>%
+  full_join(bsi_v2) %>%
+  full_join(pfracs_v2) %>%
+  full_join(pigments_v2)
+glimpse(master_v1)
