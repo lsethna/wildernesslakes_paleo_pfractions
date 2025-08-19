88ea02370ffc960f35b49eaec44023f75cee3e4e
Lienne Sethna <30530947+lsethna@users.noreply.github.com>
2025-08-19 16:20:18 -0500

    Exploring tidypaleo package as a way to make ggplot style strat diagrams

--- Stratigraphic diagrams/WL_paleo_stratdiagrams_MSfigs.R
+++ Stratigraphic diagrams/WL_paleo_stratdiagrams_MSfigs.R
@@ -4,7 +4,7 @@
 rm(list=ls())
 
 #install.packages(librarian)
-librarian::shelf(tidyverse, googledrive,readxl,rioja)
+librarian::shelf(tidyverse,rioja,tidypaleo)
 
 getwd()
 setwd("C:/Users/lsethna_smm/Documents/GitHub/wildernesslakes_paleo_pfractions") #change this to match local GitHub folder
@@ -13,18 +13,18 @@ setwd("C:/Users/lsethna_smm/Documents/GitHub/wildernesslakes_paleo_pfractions") 
 # Read in data ----
 ## ----------------------------------- ##
 
-#interpolated master dat
-int_master_dat <- read.csv("raw_data/interpolated_master_dat_10Dec24.csv")
-glimpse(int_master_dat)
+#master dat
+master_dat <- read.csv("Master dataset/WL_paleo_masterdataset_20Nov2024.csv")
+glimpse(master_dat)
 
 ## ----------------------------------- ##
 # Select data for strat plots ----
 ## ----------------------------------- ##
 
 #select variables for algae strat diagram
-paleo_prod <- int_master_dat %>% select(c(lake,year_loess,
-                                          chl_b,fuco,b_car,lutein,diato,allo,myxo,cantha,echine,
-                                          aul_subarctica,aul_ambigua,pse_elliptica,ast_formosa,tab_flocculosa,fra_crotonensis,sta_venter))
+paleo_prod <- master_dat %>% select(c(lake,year_loess,
+                                      chl_b,fuco,b_car,lutein,diato,allo,myxo,cantha,echine,
+                                      aul_subarctica,aul_ambigua,pse_elliptica,ast_formosa,tab_flocculosa,fra_crotonensis,sta_venter))
 glimpse(paleo_prod)
 unique(paleo_prod$lake)
 
@@ -32,15 +32,29 @@ unique(paleo_prod$lake)
 ## --- strat plots with ggplot --- ##
 ## ------------------------------- ##
 
+theme_set(theme_paleo())
+
 paleo_prod %>%
   mutate(lake = factor(lake,levels=c("dunnigan","finger","flame","wtwin","burnt","smoke","elbow","etwin"),
                        labels=c("Dunnigan","Finger","Flame","W. Twin","Burnt","Smoke","Elbow","E. Twin"))) %>%
+  filter(!if_all(chl_b:sta_venter,is.na)) %>% #remove rows where all paleo data is NA
   pivot_longer(cols=chl_b:sta_venter) %>%
+  mutate(name = factor(name,
+                       levels=c("chl_b","fuco","b_car","lut_zea","diato","allo","myxo","cantha","echine",
+                                "aul_subarctica","aul_ambigua","pse_elliptica","ast_formosa","tab_flocculosa",
+                                "fra_crotonensis","sta_venter"))) %>%
+  filter(!is.na(name)) %>% #remove rows where "name" is NA
   ggplot(aes(y=value,x=year_loess))+
-  geom_area()+
   coord_flip()+
-  facet_grid(rows=vars(lake),cols=vars(name))+
-  theme_classic()
+  geom_area(color="black",fill="gray")+
+  geom_bar(stat="identity",width=0.5)+
+  facet_geochem_gridh(vars(name),grouping=vars(lake))+
+  theme_classic()+
+  theme(strip.text=element_text(angle=45),
+        strip.background=element_blank(),
+        axis.text.x=element_text(angle=90),
+        axis.title=element_blank())
+
 
 #Need to plot lakes separately to control scales
 
@@ -48,116 +62,25 @@ paleo_prod %>%
 ## --- strat plots with rioja --- ##
 ## ------------------------------ ##
 
-paleo_prod_long <- 
-  paleo_prod %>%
-  pivot_longer(cols=chl_b:sta_venter) %>%
-  mutate(name = factor(name,
-                       levels=c("chl_b","fuco","b_car","lut_zea","diato","allo","myxo","cantha","echine",
-                                "aul_subarctica","aul_ambigua","pse_elliptica","ast_formosa","tab_flocculosa",
-                                "fra_crotonensis","sta_venter")))
-#plot theme for all 
-plot_theme <- 
-  theme_classic()+
-  theme(strip.background=element_blank(),
-        strip.text=element_text(angle=45),
-        axis.text.x=element_text(angle=90),
-        panel.spacing=unit(1, "lines"))
+#finger stratigraph
+finger <- paleo_prod %>%
+  filter(lake=="finger")
+colnames(finger)
+finger_plot <- strat.plot(finger[,3:18],yvar=finger$year_loess,
+                          y.tks=seq(1860,2020,20),
+                          plot.poly=T,plot.bar=T,col.bar="black",
+                          srt.xlabel=30,title="Finger")
+#finger CONISS
+#distance matrix  
+dist.mat <- vegdist(finger[,2:9],method="euclidian", binary=FALSE, diag=FALSE, upper=FALSE, na.rm=T)
+#coniss cluster
+chclust.obj <- chclust(dist.mat,method="coniss")
+#find optimal number of clusters
+bstick(chclust.obj)
 
-#Dunnigan
-dunnigan_paleo <- paleo_prod_long %>%
-  filter(lake=="dunnigan") %>%
-  ggplot(aes(y=value,x=year_loess))+
-  geom_area(color="black",fill="gray")+
-  geom_bar(stat="identity",width=0.5)+
-  coord_flip()+
-  scale_x_continuous(breaks=seq(1860,2020,20))+
-  facet_wrap(~name,ncol=16,scales="free_x")+
-  labs(x="Year",title="Dunnigan (well-mixed)")+
-  plot_theme
-
-#Finger
-finger_paleo <- paleo_prod_long %>%
-  filter(lake=="finger") %>%
-  ggplot(aes(y=value,x=year_loess))+
-  geom_area(color="black",fill="gray")+
-  geom_bar(stat="identity",width=0.5)+
-  coord_flip()+
-  scale_x_continuous(breaks=seq(1860,2020,20))+
-  facet_wrap(~name,ncol=16,scales="free_x")+
-  labs(x="Year",title="Finger (well-mixed)")+
-  plot_theme
-
-#Flame
-flame_paleo <- paleo_prod_long %>%
-  filter(lake=="flame") %>%
-  ggplot(aes(y=value,x=year_loess))+
-  geom_area(color="black",fill="gray")+
-  geom_bar(stat="identity",width=0.5)+
-  coord_flip()+
-  scale_x_continuous(breaks=seq(1860,2020,20))+
-  facet_wrap(~name,ncol=16,scales="free_x")+
-  labs(x="Year",title="Flame (dimictic)")+
-  plot_theme
-
-#West Twin
-wtwin_paleo <- paleo_prod_long %>%
-  filter(lake=="wtwin") %>%
-  ggplot(aes(y=value,x=year_loess))+
-  geom_area(color="black",fill="gray")+
-  geom_bar(stat="identity",width=0.5)+
-  coord_flip()+
-  scale_x_continuous(breaks=seq(1860,2020,20))+
-  facet_wrap(~name,ncol=16,scales="free_x")+
-  labs(x="Year",title="West Twin (dimictic)")+
-  plot_theme
-
-#Burnt
-burnt_paleo <- paleo_prod_long %>%
-  filter(lake=="burnt") %>%
-  ggplot(aes(y=value,x=year_loess))+
-  geom_area(color="black",fill="gray")+
-  geom_bar(stat="identity",width=0.5)+
-  coord_flip()+
-  scale_x_continuous(breaks=seq(1860,2020,20))+
-  facet_wrap(~name,ncol=16,scales="free_x")+
-  labs(x="Year",title="Burnt (polymictic)")+
-  plot_theme
-  
-#Smoke
-smoke_paleo <- paleo_prod_long %>%
-  filter(lake=="smoke") %>%
-  ggplot(aes(y=value,x=year_loess))+
-  geom_area(color="black",fill="gray")+
-  geom_bar(stat="identity",width=0.5)+
-  coord_flip()+
-  scale_x_continuous(breaks=seq(1860,2020,20))+
-  facet_wrap(~name,ncol=16,scales="free_x")+
-  labs(x="Year",title="Smoke (polymictic)")+
-  plot_theme
-
-#Elbow
-elbow_paleo <- paleo_prod_long %>%
-  filter(lake=="elbow") %>%
-  ggplot(aes(y=value,x=year_loess))+
-  geom_area(color="black",fill="gray")+
-  geom_bar(stat="identity",width=0.5)+
-  coord_flip()+
-  scale_x_continuous(breaks=seq(1860,2020,20))+
-  facet_wrap(~name,ncol=16,scales="free_x")+
-  labs(x="Year",title="Elbow (polymictic)")+
-  plot_theme
-
-#East Twin
-etwin_paleo <- paleo_prod_long %>%
-  filter(lake=="etwin") %>%
-  ggplot(aes(y=value,x=year_loess))+
-  geom_area(color="black",fill="gray")+
-  geom_bar(stat="identity",width=0.5)+
-  coord_flip()+
-  scale_x_continuous(breaks=seq(1860,2020,20))+
-  facet_wrap(~name,ncol=16,scales="free_x")+
-  labs(x="Year",title="East Twin (polymictic)")+
-  plot_theme
+cutree(chclust.obj,k=3)
+#finger=3
+addClustZone(finger_plot,chclust.obj,nZone=3,lwd=1.5,lty="dashed",col="gray25")
 
 ## ------------------------ ##
 ## --- cowplot together --- ##
