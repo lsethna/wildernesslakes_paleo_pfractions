9f637bc1676942c3c0af9c15ebf4bec9a92572f9
Lienne Sethna <30530947+lsethna@users.noreply.github.com>
2025-09-17 17:15:13 -0500

    Recreate strat plots with CONISS for sig bio variables from PCoA

--- Bio proxy CONISS/Diatom_pigment CONISS with geochem strat plots.R
+++ Bio proxy CONISS/Diatom_pigment CONISS with geochem strat plots.R
@@ -4,7 +4,7 @@
 rm(list=ls())
 
 #install.packages(librarian)
-librarian::shelf(tidyverse, readxl,rioja)
+librarian::shelf(tidyverse, readxl,rioja,vegan)
 
 getwd()
 setwd("C:/Users/lsethna_smm/Documents/GitHub/wildernesslakes_paleo_pfractions") #change this to match local GitHub folder
@@ -12,8 +12,8 @@ setwd("C:/Users/lsethna_smm/Documents/GitHub/wildernesslakes_paleo_pfractions") 
 ## ----------------------------------- ##
 # Read in data ----
 ## ----------------------------------- ##
-#master data
-master_dat <- read.csv("raw_data/WL_paleo_masterdataset_20Nov2024.csv") %>% select(!X)
+#interpolated master data
+master_dat <- read.csv("raw_data/interpolated_master_dat_4Sep25.csv") %>% select(!X)
 glimpse(master_dat)
 #Organic C variable; keep only lake, date,and org_C variables
 org_c <- read.csv("raw_data/deriv_orgC.csv") %>% select(lake,year_loess,orgC_burial_g_cm2_yr,deriv_orgC)
@@ -25,47 +25,56 @@ glimpse(pfrac)
 
 ## merge together
 master_dat_v2 <- master_dat %>% left_join(org_c) %>% left_join(pfrac)
+glimpse(master_dat_v2)
 
-## ----------------------------------- ##
-# Select data for strat plots ----
-## ----------------------------------- ##
+## get variables from PCoA
+imp_variables <- read.csv("Paleo PCoA/sig_variables_pcoa_by_lake.csv")
+#get codes to change imp_variables back to match var names in master data
+variable_code <- read.csv("Paleo PCoA/pcoa_variable_abbr.csv")
 
+## --------------------------------------------------------------------------- ##
+## ---------------------------- strat plots ---------------------------------- ##
+## --------------------------------------------------------------------------- ##
 
-#Burnt Lake plot
-burnt_paleo <- paleo %>%
-  filter(lake=="burnt")
-burnt.paleo <- strat.plot(burnt_paleo[1:27,7:18],yvar=burnt_paleo$year_loess[1:27], #burnt[rows,col]
-                          y.tks=seq(1860,2020,20),
-                          plot.poly=T,plot.bar=T,col.bar="black",
-                          srt.xlabel=45,title="Burnt")
-smoke_paleo <- paleo %>%
-  filter(lake=="smoke")
-smoke.paleo <- strat.plot(smoke_paleo[1:27,7:18],yvar=smoke_paleo$year_loess[1:27], #burnt[rows,col]
-                          y.tks=seq(1860,2020,20),
-                          plot.poly=T,plot.bar=T,col.bar="black",
-                          srt.xlabel=45,title="Smoke")
-etwin_paleo <- paleo %>%
-  filter(lake=="etwin")
-etwin.paleo <- strat.plot(etwin_paleo[1:71,7:18],yvar=etwin_paleo$year_loess[1:71], #burnt[rows,col]
-                          y.tks=seq(1860,2020,20),
-                          plot.poly=T,plot.bar=T,col.bar="black",
-                          srt.xlabel=45,title="etwin")
-#plot using rioja?
-librarian::shelf(vegan)
+lakes = unique(master_dat_v2$lake)
 
-#Burnt
-burnt <- wrc %>%
-  filter(lake=="burnt")
-glimpse(burnt)
-burnt.plot <- strat.plot(burnt[1:32,3:17],yvar=burnt$year_loess[1:32], #burnt[rows,col]
-                         y.tks=seq(1860,2020,20),
-                         plot.poly=T,plot.bar=T,col.bar="black",
-                         srt.xlabel=45,title="Burnt")
-#flame
-flame <- wrc %>%
-  filter(lake=="flame")
-glimpse(flame)
-flame.plot <- strat.plot(flame[1:21,3:11],yvar=flame$year_loess[1:21],
-                         y.tks=seq(1850,2015,15),
-                         plot.poly=T,plot.bar=T,col.bar="black",
-                         srt.xlabel=45,title="Flame")
+for (i in 1:length(lakes)){
+# Select data for strat plots
+vars_code <- imp_variables %>% filter(lake==lakes[i]) %>% select(var) %>% pull()
+#get full name from code df
+vars <- variable_code %>% filter(code %in% vars_code) %>% select(var) %>% pull()
+
+lake_dat <- master_dat_v2 %>% filter(lake==lakes[i]) %>%
+  select(year_loess,all_of(vars),deriv_orgC,labile,ironbound)
+
+#CONISS
+#get column numbers to skip
+col_names_to_skip <- c("year_loess","deriv_orgC","labile","ironbound")
+col_n_to_skip <- which(names(lake_dat) %in% col_names_to_skip)
+#distance matrix  
+dist.mat <- vegdist(lake_dat[,-c(col_n_to_skip)],method="euclidian", binary=FALSE, diag=FALSE, upper=FALSE, na.rm=T)
+#coniss cluster
+chclust.obj <- chclust(dist.mat,method="coniss")
+
+##here we can define the types of variables for color
+#and pretty them up for plotting
+#will also need to pretty up the lake names for the titles
+
+#run this to save plot
+file = paste0("Bio proxy CONISS/",lakes[i],"_strat_coniss.pdf")
+
+pdf(
+  file=file,
+  width=11,height=8.5
+)
+
+strat.plot(lake_dat[,-1],yvar=lake_dat$year_loess, #[rows,col]
+           y.tks=seq(1830,2020,20),
+           plot.poly=T,plot.bar=T,col.bar="black",
+           srt.xlabel=45,title=paste(lakes[i]),
+           las=2,mgp=c(3,1,0.25),xSpace=0.01,
+           clust=chclust.obj)
+
+dev.off() #clear before rerunning next lake
+
+}
